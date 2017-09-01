bf33c7ce4af6c134a751506affc55b5199a79f22
Anchal Chaudhary <anchal.ldn5@gmail.com>
2017-09-02 01:10:35 +0530

    first commit

---
+++ SakshamEMSApi/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SakshamEMSApi.Models;
+
+namespace SakshamEMSApi.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        DBSakshamEntities db = new DBSakshamEntities();
+        [HttpPost]
+        public IHttpActionResult Register(Student model)
+        {
+            tblStudent objtblStudent = new tblStudent();
+            objtblStudent.Name = model.Name;
+            objtblStudent.StudentNo = model.StudentNo;
+            objtblStudent.Branch = model.Branch;
+            objtblStudent.Year = model.Year;
+            objtblStudent.ContactNumber = model.ContactNumber;
+            objtblStudent.SportsInterested = model.SportsInterested;
+            objtblStudent.Hosteler = model.Hosteler;
+
+            db.tblStudents.Add(objtblStudent);
+            db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ SakshamEMSApi/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SakshamEMSApi.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string StudentNo { get; set; }
+        public string Branch { get; set; }
+        public string Year { get; set; }
+        public string ContactNumber { get; set; }
+        public string SportsInterested { get; set; }
+        public string Hosteler { get; set; }
+    }
+}
